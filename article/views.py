315db371c948c017bbb24a71bfeda905a7ab7792
pavelwen <pavelwen@gmail.com>
2015-07-03 17:42:58 +0800

    first git control

---
+++ article/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+# Create your views here.
+
+
+def home(request):
+    post_list = Article.objects.all()  # get all article
+    return render(request, 'home.html', {'post_list': post_list})
+
+
+def detail(request, my_args):
+    post = Article.objects.all()[int(my_args)]
+    str = ("title = %s, category = %s, date_time = %s, content = %s"
+           % (post.title, post.category, post.date_time, post.content))
+    return HttpResponse(str)
+
+def test(request):
+    return render(request, 'test.html')
